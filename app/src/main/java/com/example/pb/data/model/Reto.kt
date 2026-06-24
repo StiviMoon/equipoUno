7b7d43d0d7f6077c508f8e0771e03d0e55a51c2e
DAMN05 <danielmaigual00@gmail.com>
2026-06-24 02:50:15 -0500

    feat(data): crear modelo Reto y Repositorio de Firestore con sync en tiempo real

---
+++ app/src/main/java/com/example/pb/data/model/Reto.kt
@@ -0,0 +1,8 @@
+package com.example.pb.data.model
+
+data class Reto(
+    val id: String = "",
+    val descripcion: String = "",
+    val uidUsuario: String = "",
+    val fechaCreacion: Long = 0
+)
